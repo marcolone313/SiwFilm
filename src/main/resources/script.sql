92c522f850c9fa278e4d1ea8ceb3c1b0c65c6902
m.giorgi <m.giorgi@oscinnovation.com>
2023-07-03 16:06:46 +0200

    Commit Progetto Finito

--- src/main/resources/script.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO artist (id, name, surname, birth_date) values(0, 'James', 'Cameron', '13-11-1997');
-INSERT INTO artist (id, name, surname, birth_date) values(1, 'Francis Ford', 'Coppola', '13-11-1997');
-INSERT INTO artist (id, name, surname, birth_date) values(2, 'Peter', 'Jackson', '13-11-1997');
-INSERT INTO artist (id, name, surname, birth_date) values(3, 'Ron', 'Clements', '13-11-1997');
-INSERT INTO artist (id, name, surname, birth_date) values(4, 'Quentin', 'Tarantino', '13-11-1997');
-INSERT INTO artist (id, name, surname, birth_date) values(5, 'Brian', 'De Palma', '13-11-1997');
-INSERT INTO artist (id, name, surname, birth_date) values(6, 'George', 'Lucas', '13-11-1997');	
-INSERT INTO artist (id, name, surname, birth_date) values(7, 'John', 'Lasseter', '13-11-1997');
-INSERT INTO artist (id, name, surname, birth_date) values(8, 'Al', 'Pacino', '13-11-1997');
